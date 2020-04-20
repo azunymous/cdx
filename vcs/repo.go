5351d394d2ad1548ffdf1d8a222b836246ebfaf8
azunymous <azu@azunymo.us>
2020-04-20 19:46:35 +0100

    Implement function for getting tags from HEAD

--- vcs/repo.go
+++ vcs/repo.go
@@ -3,6 +3,7 @@ package vcs
 import (
 	"github.com/go-git/go-billy/v5"
 	"github.com/go-git/go-git/v5"
+	"github.com/go-git/go-git/v5/plumbing"
 	"github.com/go-git/go-git/v5/plumbing/cache"
 	filesystem2 "github.com/go-git/go-git/v5/storage/filesystem"
 )
@@ -17,4 +18,24 @@ func NewRepo(fs billy.Filesystem) (*Repo, error) {
 		return nil, err
 	}
 	return &Repo{gitRepo: gr}, nil
+}
+
+func (r *Repo) TagsForHead() (map[string]struct{}, error) {
+	current, err := r.gitRepo.ResolveRevision("HEAD")
+	if err != nil {
+		return nil, err
+	}
+	tags, err := r.gitRepo.Tags()
+	if err != nil {
+		return nil, err
+	}
+
+	t := map[string]struct{}{}
+	_ = tags.ForEach(func(reference *plumbing.Reference) error {
+		if reference.Hash() == *current {
+			t[reference.Name().Short()] = struct{}{}
+		}
+		return nil
+	})
+	return t, nil
 }
--- vcs/repo_test.go
+++ vcs/repo_test.go
@@ -24,16 +24,77 @@ func TestNewRepo(t *testing.T) {
 	check.Ok(t, err)
 }
 
-func createGitRepo(fs billy.Filesystem) {
-	r, _ := git.Init(filesystem2.NewStorage(fs, cache.NewObjectLRUDefault()), fs)
-	w, _ := r.Worktree()
+func TestTagsForHead(t *testing.T) {
+	fs := memfs.New()
+	createGitRepo(fs)
+	createVersionTag(fs, "app-0.0.1")
 
+	repo, err := NewRepo(fs)
+	check.Ok(t, err)
+	tagsForHead, err := repo.TagsForHead()
+	check.Ok(t, err)
+	check.Equals(t, 1, len(tagsForHead))
+
+	_, ok := tagsForHead["app-0.0.1"]
+	check.Assert(t, ok, "Expected 'app-0.0.1' in map")
+}
+
+func TestTagsForHeadForMultipleTags(t *testing.T) {
+	fs := memfs.New()
+	createGitRepo(fs)
+	createVersionTag(fs, "app-0.0.1-alpha")
+	createVersionTag(fs, "app-0.0.1")
+
+	repo, err := NewRepo(fs)
+	check.Ok(t, err)
+	tagsForHead, err := repo.TagsForHead()
+	check.Ok(t, err)
+	check.Equals(t, 2, len(tagsForHead))
+	check.Equals(t, map[string]struct{}{"app-0.0.1-alpha": {}, "app-0.0.1": {}}, tagsForHead)
+}
+
+func TestTagsForHeadForMultipleCommits(t *testing.T) {
+	fs := memfs.New()
+	createGitRepo(fs)
+	createVersionTag(fs, "app-0.0.1")
+	createCommit(fs, "New Version", "Hello world 2")
+	createVersionTag(fs, "app-0.0.2")
+
+	repo, err := NewRepo(fs)
+	check.Ok(t, err)
+	tagsForHead, err := repo.TagsForHead()
+	check.Ok(t, err)
+	check.Equals(t, 1, len(tagsForHead))
+
+	_, ok := tagsForHead["app-0.0.2"]
+	check.Assert(t, ok, "Expected 'app-0.0.2' in map")
+}
+
+func TestTagsForHeadWhenEmpty(t *testing.T) {
+	fs := memfs.New()
+	createGitRepo(fs)
+
+	repo, err := NewRepo(fs)
+	check.Ok(t, err)
+	tagsForHead, err := repo.TagsForHead()
+	check.Ok(t, err)
+	check.Equals(t, 0, len(tagsForHead))
+}
+
+func createGitRepo(fs billy.Filesystem) {
+	_, _ = git.Init(filesystem2.NewStorage(fs, cache.NewObjectLRUDefault()), fs)
+	createCommit(fs, "example-git-file", "hello world!")
+}
+
+func createCommit(fs billy.Filesystem, filename, msg string) {
+	r, _ := git.Open(filesystem2.NewStorage(fs, cache.NewObjectLRUDefault()), fs)
+	w, _ := r.Worktree()
 	// create a new file inside of the worktree of the project using the go standard library.
-	file, _ := fs.Create("example-git-file")
-	_, _ = file.Write([]byte("hello world!"))
+	file, _ := fs.Create(filename)
+	_, _ = file.Write([]byte(msg))
 
 	// Adds the new file to the staging area.
-	_, _ = w.Add("example-git-file")
+	_, _ = w.Add(filename)
 
 	// Commits the current staging area to the repository, with the new file
 	// just created.
@@ -44,5 +105,10 @@ func createGitRepo(fs billy.Filesystem) {
 			When:  time.Now(),
 		},
 	})
+}
 
+func createVersionTag(fs billy.Filesystem, tag string) {
+	r, _ := git.Open(filesystem2.NewStorage(fs, cache.NewObjectLRUDefault()), fs)
+	revision, _ := r.ResolveRevision("HEAD")
+	_, _ = r.CreateTag(tag, *revision, nil)
 }
