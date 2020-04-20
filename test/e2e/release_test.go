502f251a038b9b1c414f3c57672421f2bdd2943b
azunymous <azu@azunymo.us>
2020-04-20 22:20:05 +0100

    Add additional end to end tests for release repository

---
+++ test/e2e/release_test.go
@@ -0,0 +1,46 @@
+//+build e2e
+
+package e2e
+
+import (
+	"cdx/internal/check"
+	"io/ioutil"
+	"os"
+	"os/exec"
+	"strings"
+	"testing"
+)
+
+func TestReleaseCommandExists(t *testing.T) {
+	command := exec.Command("cdx", "tag", "release", "-n", "app")
+	err := command.Run()
+	check.Ok(t, err)
+}
+
+func TestReleaseOpensRepository(t *testing.T) {
+	createTempGitDir(t)
+	command := exec.Command("cdx", "tag", "release", "-n", "app")
+	err := command.Run()
+	check.Ok(t, err)
+}
+
+func TestReleaseTagsRepository(t *testing.T) {
+	createTempGitDir(t)
+	command := exec.Command("cdx", "tag", "release", "-n", "app")
+	err := command.Run()
+	check.Ok(t, err)
+	output, err := exec.Command("git", "tag", "--points-at", "HEAD").CombinedOutput()
+	check.Ok(t, err)
+	check.Equals(t, "app-0.1.0", strings.TrimSpace(string(output)))
+}
+
+func createTempGitDir(t *testing.T) string {
+	fn, err := ioutil.TempDir(os.TempDir(), "cdx-test-")
+	check.Ok(t, err)
+	_ = os.Chdir(fn)
+	_, _ = exec.Command("git", "init").CombinedOutput()
+	_, _ = exec.Command("bash", "-c", "echo 'hello world' > file.txt").CombinedOutput()
+	_, _ = exec.Command("git", "add", "file.txt").CombinedOutput()
+	_, _ = exec.Command("git", "commit", "-m", `"Commit 1"`).CombinedOutput()
+	return fn
+}
--- test/e2e/tag_test.go
+++ test/e2e/tag_test.go
@@ -3,6 +3,7 @@
 package e2e
 
 import (
+	"cdx/internal/check"
 	"os/exec"
 	"testing"
 )
@@ -10,8 +11,5 @@ import (
 func TestTagCommandExists(t *testing.T) {
 	command := exec.Command("cdx", "tag", "-n", "app")
 	err := command.Run()
-
-	if err != nil {
-		t.Fatalf("Expected no error, got %v", err)
-	}
+	check.Ok(t, err)
 }
