b34b3defb4c208772bc5fecbb8cacd12ad153f57
azunymous <azu@azunymo.us>
2020-05-02 17:25:53 +0100

    Remove basic auth and no git environment variable override.
    There is now a requirement to use git when the push (--push) flag is used with
    release or promote.
    
    This implementation and requirement significantly simplifies pushing tags for
    git as it does not require implementing the configuration required for using
    or providing SSH keys or basic authentication. However it likely breaks
    Windows compatibility.
    
    As this tool is primarily intended to run on CI/CD systems which are often
    containerized or virtualized, this breaking change should not cause any
    harm.

--- vcs/gogit/push.go
+++ vcs/gogit/push.go
@@ -2,30 +2,15 @@ package gogit
 
 import (
 	"context"
-	"github.com/go-git/go-git/v5"
-	"github.com/go-git/go-git/v5/config"
-	"github.com/go-git/go-git/v5/plumbing/transport"
-	"github.com/go-git/go-git/v5/plumbing/transport/http"
 	"github.com/sirupsen/logrus"
-	"os"
 	"os/exec"
-	"strings"
 	"time"
 )
-
-const basicAuthEnv = "CDX_BASIC_AUTH"
-const noGitEnv = "CDX_NO_GIT"
 
 func (r *Repo) PushTags() error {
 	ctx, cancelFunc := context.WithTimeout(context.Background(), 30*time.Second)
 	defer cancelFunc()
 
-	if os.Getenv(basicAuthEnv) != "" || os.Getenv(noGitEnv) != "" {
-		rs := config.RefSpec("refs/tags/*:refs/tags/*")
-		return r.gitRepo.PushContext(
-			ctx,
-			&git.PushOptions{RefSpecs: []config.RefSpec{rs}, Auth: getAuth()})
-	}
 	logrus.Info("Pushing using git command")
 	cmd := exec.CommandContext(ctx, "git", "push", "--tags", "-q")
 
@@ -33,21 +18,6 @@ func (r *Repo) PushTags() error {
 	logrus.Info(string(out))
 	if err != nil {
 		return err
-	}
-	return nil
-}
-
-func getAuth() transport.AuthMethod {
-	if os.Getenv(basicAuthEnv) != "" {
-		auth := strings.SplitN(os.Getenv(basicAuthEnv), ":", 2)
-		if len(auth) < 2 || auth[0] == "" || auth[1] == "" {
-			logrus.Fatal("Invalid basic auth provided")
-		}
-		logrus.Info("Using basic auth:", auth)
-		return &http.BasicAuth{
-			Username: auth[0],
-			Password: auth[1],
-		}
 	}
 	return nil
 }
