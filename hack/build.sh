0006a130e770e01d0b8822db8a457a02cce3c82f
azunymous <azu@azunymo.us>
2020-05-10 18:55:03 +0100

    Change build script to use fallback hash if version is untagged
    This also makes sure the version is from the current HEAD commit

--- hack/build.sh
+++ hack/build.sh
@@ -5,4 +5,6 @@ PROJECT_DIR="${SCRIPT_DIR}/../"
 
 COMMAND=${1:-build}
 
-go ${COMMAND} -ldflags "-X main.version=`cdx tag latest -n cdx` -X main.buildTime=`date -u '+%Y-%m-%d_%I:%M:%S%p'` -X main.gitHash=`git rev-parse HEAD`" ./cmd/cdx/
+go ${COMMAND} -ldflags \
+  "-X main.version=`cdx tag latest -n cdx --head --fallback` -X main.buildTime=`date -u '+%Y-%m-%d_%I:%M:%S%p'` -X main.gitHash=`git rev-parse HEAD`" \
+  ./cmd/cdx/
