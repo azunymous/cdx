858f27fb1a0e54193067a206a8dc573f86eebe71
azunymous <azu@azunymo.us>
2020-05-15 23:02:43 +0100

    Use go-cache as backing database for sharing commits quickly
    This is ideal if the cdx share server is running as one instance as the
    data does not need to be persistent. If the server needs to be
    distributed another choice would likely be better.
    
    This should be hidden behind an interface so it can be replaced with
    a fake.

--- go.mod
+++ go.mod
@@ -6,6 +6,7 @@ require (
 	github.com/go-git/go-billy/v5 v5.0.0
 	github.com/go-git/go-git/v5 v5.0.0
 	github.com/golang/protobuf v1.4.0
+	github.com/patrickmn/go-cache v2.1.0+incompatible
 	github.com/sirupsen/logrus v1.2.0
 	github.com/spf13/cobra v1.0.0
 	google.golang.org/grpc v1.28.1
--- watch/server.go
+++ watch/server.go
@@ -4,47 +4,60 @@ import (
 	"context"
 	"fmt"
 	"github.com/azunymous/cdx/watch/diff"
+	"github.com/patrickmn/go-cache"
 	"github.com/sirupsen/logrus"
 	"google.golang.org/grpc"
 	"log"
 	"net"
+	"time"
 )
 
+const defaultExpiration = 5 * time.Minute
+const cleanupInterval = 10 * time.Minute
+
 type DiffServer struct {
+	db *cache.Cache
 }
 
 func NewServer() error {
+	c := cache.New(defaultExpiration, cleanupInterval)
+	srv := &DiffServer{db: c}
 	lis, err := net.Listen("tcp", fmt.Sprintf(":%d", 19443))
 	if err != nil {
 		log.Fatalf("failed to listen: %v", err)
 	}
 	grpcServer := grpc.NewServer()
-	diff.RegisterDiffServer(grpcServer, &DiffServer{})
+	diff.RegisterDiffServer(grpcServer, srv)
 	return grpcServer.Serve(lis)
 }
 
-func (d DiffServer) SendDiff(ctx context.Context, request *diff.DiffRequest) (*diff.DiffCommits, error) {
+func (d *DiffServer) SendDiff(ctx context.Context, request *diff.DiffRequest) (*diff.DiffCommits, error) {
 	logrus.Infof("Received message %s", request.Name)
-	commits, err := getDiff(request.GetName())
+	commits, err := d.getDiff(request.GetName())
 	if err != nil {
 		return nil, err
 	}
 	return &diff.DiffCommits{Commits: commits}, nil
 }
 
-func (d DiffServer) UploadDiff(ctx context.Context, reply *diff.DiffCommits) (*diff.DiffConfirm, error) {
+func (d *DiffServer) UploadDiff(ctx context.Context, reply *diff.DiffCommits) (*diff.DiffConfirm, error) {
 	logrus.Infof("Received message %s", reply.Name)
-	err := saveDiff(reply.GetName(), reply.GetCommits())
+	err := d.saveDiff(reply.GetName(), reply.GetCommits())
 	if err != nil {
 		return nil, err
 	}
 	return &diff.DiffConfirm{}, nil
 }
 
-func getDiff(name string) (string, error) {
-	return "", nil
+func (d *DiffServer) getDiff(name string) (string, error) {
+	get, ok := d.db.Get(name)
+	if !ok {
+		return "", fmt.Errorf("no patch with name %s found", name)
+	}
+	return get.(string), nil
 }
 
-func saveDiff(name string, commits string) error {
+func (d *DiffServer) saveDiff(name string, commits string) error {
+	d.db.Set(name, commits, cache.DefaultExpiration)
 	return nil
 }
